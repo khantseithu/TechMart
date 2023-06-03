e6b6b85a9d2a21b37b2a96c8e2841895e3a3f837
Khant Sithu <khantseithuu@gmail.com>
2023-06-03 11:17:26 +0000

    feat: Routes setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,10 +6,26 @@ import './assets/styles/bootstrap.custom.css';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+    </Route>
+  )
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
