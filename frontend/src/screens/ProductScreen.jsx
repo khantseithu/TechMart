66a56458eaa49bf52261dae802abb5e6ebfe5e11
Khant Sithu <khantseithuu@gmail.com>
2023-06-05 04:33:07 +0000

    feat: Fetch products from backend

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -2,10 +2,24 @@ import { useParams, Link } from 'react-router-dom';
 import products from '../products';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const ProductScreen = () => {
   const { id: ProductId } = useParams();
-  const product = products.find((p) => p._id === ProductId);
+  //   const product = products.find((p) => p._id === ProductId);
+  const [product, setProduct] = useState({});
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(
+        `https://khantseithu-potential-train-p594gqv6x5636457-5000.preview.app.github.dev/api/products/${ProductId}`
+      );
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [ProductId]);
+
   return (
     <>
       <Link to="/" className="btn btn-light my-3">
