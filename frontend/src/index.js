ecaba119372e869ee09afa4c0a19dc86ba9fc258
Khant Sithu <khantseithuu@gmail.com>
2023-06-05 03:03:12 +0000

    feat: Build rating stars

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
-import './index.css';
+import './assets/styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 import {
