30916a325e561207388c769c34e4b078ece5a621
Khant Sithu <khantseithuu@gmail.com>
2023-06-05 16:00:23 +0000

    feat: Error handling

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 // import products from './data/products.js';
 import productRoutes from './routes/productRoutes.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
@@ -10,6 +11,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 app.use(cors());
+app.use(notFound);
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
