f91d3e1a5807728a41da10afb445025023ef969b
Khant Sithu <khantseithuu@gmail.com>
2023-06-05 04:26:48 +0000

    feat: Fetch products from the backend

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,8 +1,23 @@
 import { Row, Col } from 'react-bootstrap';
-import products from '../products';
+// import products from '../products';
 import Product from '../components/Product';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get(
+        'https://khantseithu-potential-train-p594gqv6x5636457-5000.preview.app.github.dev/api/products'
+      );
+      console.log(data);
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
+  console.log(products);
   return (
     <>
       <h1>Latest Products</h1>
