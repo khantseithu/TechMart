18cba024ea96f8cb6c185aa418833fa9c0ec56f8
Khant Sithu <khantseithuu@gmail.com>
2023-06-07 07:13:25 +0000

    feat: Build Cart Screen & remove from cart action

---
+++ frontend/src/screens/CartScreen.jsx
@@ -0,0 +1,107 @@
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate } from 'react-router-dom';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
+import Message from '../components/Message';
+import { FaTrash } from 'react-icons/fa';
+
+const CartScreen = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+  return (
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: '20px' }}>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row style={{ alignItems: 'center' }}>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                    <Form.Control
+                      as="select"
+                      value={item.qty}
+                      onChange={(e) =>
+                        dispatch(addToCart(item._id, Number(e.target.value)))
+                      }
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option
+                          key={x + 1}
+                          value={x + 1}
+                          style={{ color: 'black' }}
+                        >
+                          {x + 1}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => dispatch(removeFromCart(item._id))}
+                    >
+                      <FaTrash />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card style={{ marginBottom: '20px' }}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2 style={{ textAlign: 'center' }}>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              <h3 style={{ textAlign: 'center' }}>
+                $
+                {cartItems
+                  .reduce((acc, item) => acc + item.qty * item.price, 0)
+                  .toFixed(2)}
+              </h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="btn-block"
+                disabled={cartItems.length === 0}
+                onClick={() => navigate('/login?redirect=shipping')}
+              >
+                Proceed To Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
+};
+export default CartScreen;
--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -11,11 +11,29 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const item = action.payload;
+      const existItem = state.cartItems.find((x) => x._id === item._id);
+      if (existItem) {
+        state.cartItems = state.cartItems.map((x) =>
+          x._id === existItem._id ? item : x
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
       const updatedCart = updateCart(state, item);
+      return updatedCart;
+    },
+    removeFromCart: (state, action) => {
+      // filter out the item that matches the payload id
+      state.cartItems = state.cartItems.filter(
+        (item) => item._id !== action.payload
+      );
+
+      // update the cart
+      const updatedCart = updateCart(state);
       return updatedCart;
     },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
