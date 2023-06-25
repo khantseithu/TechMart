4aac1e3a7d68ae3a441fcf8200ea18c9d97e5b9f
Khant Sithu <khantseithuu@gmail.com>
2023-06-25 03:41:11 +0000

    bugfix: base url(using proxy), remove cors (jwt http only)

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 import connectDB from './config/db.js';
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,7 +1,7 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'development'
-    ? 'https://khantseithu-potential-train-p594gqv6x5636457-5000.preview.app.github.dev'
-    : 'https://mern-ecommerce-website.herokuapp.com';
+export const BASE_URL = "";
+  // process.env.NODE_ENV === 'development'
+  //   ? 'http://localhost:5000'
+  //   : 'https://mern-ecommerce-website.herokuapp.com';
 
 export const PRODUCTS_URL = `/api/products`;
 export const USERS_URL = `/api/users`;
